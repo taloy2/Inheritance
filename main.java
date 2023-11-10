41672c57230e4fe370e4ea81ee8bc98b67d52267
taloy2 <sinabiananrey@gmail.com>
2023-11-10 21:23:21 +0800

    main added

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+      
+        Manok myManok = new Manok("Ang Manok");
+        
+        myManok.kaon();
+        myManok.sabong();
+        myManok.Pilde();
+        myManok.Sabaw();
+        
+        
+                
+    }
+    
+}
